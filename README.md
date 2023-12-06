a89a771b2cd6881cf407581d62280ed1fbb1553b
Jasmeet Matta <jashoney99@gmail.com>
2023-12-06 13:20:11 +0000

    UI upgrade, new features, sound, high score

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# reaction-tester
+# Reaction Tester Game
+
+Test your reaction time by clicking on randomly appearing shapes as quickly as possible!
+
+## Description
+
+This simple web-based game measures your reaction time by displaying randomly generated shapes on the screen. The goal is to click on the shapes as quickly as you can after they appear. The game calculates the time taken to click each shape and displays it to the user.
+
+## Features
+
+- Randomly generated shapes (circles or squares)
+- Measure reaction time on click
+- Display fastest reaction time achieved
+- Confetti animation on achieving a new fastest time
+- Responsive layout
+
+## How to Play
+
+1. Open the game in your web browser.
+2. Click on the shapes as soon as they appear.
+3. The timer will track your reaction time for each click.
+4. Try to achieve the fastest reaction time!
+
+## Usage
+
+- Clone the repository.
+- Open `index.html` in your web browser.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Author
+
+- [Jasmeet Matta](https://github.com/jasmeet-matta)
+
+## Acknowledgements
+
+- Confetti animation library used: [canvas-confetti](https://github.com/catdad/canvas-confetti)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- script.js
+++ script.js
@@ -1,50 +1,121 @@
-			var start = new Date().getTime();
-			
-			function randColor(){
-				var col = '0123456789ABCDEF'.split('');
-				var color = "#";
-				
-				for(i=0; i<6; i++){
-					color += col[Math.floor(Math.random()*16)];
-				}
-				return color;
-			}
-		
-			function ShapeAppear() {
-				
-				var top = Math.random() * 300;
-				var left = Math.random() * 300;
-				var width = (Math.random() * 200)+100;
-				
-				if(Math.random() > 0.5){
-					document.getElementById("shape").style.borderRadius="50%";
-					
-				}else{
-						document.getElementById("shape").style.borderRadius="0";
-					}
-			
-				
-				document.getElementById("shape").style.height = width + "px";
-				document.getElementById("shape").style.width = width + "px";
-				document.getElementById("shape").style.top = top + "px";
-				document.getElementById("shape").style.left = left + "px";
-				document.getElementById("shape").style.backgroundColor = randColor();
-				
-				document.getElementById("shape").style.display = "block";
-				start = new Date().getTime();
-			}
-		
-			function AppearAgain(){
-				setTimeout(ShapeAppear,Math.random() * 2000);
-			}
-			
-			AppearAgain();
-			
-			document.getElementById("shape").onclick = function(){
-				document.getElementById("shape").style.display="none";
-				var end = new Date().getTime();
-				var time = (end - start)/1000;
-				document.getElementById("timeTaken").innerHTML = time + "s";
-				AppearAgain();
-			}
-		
+var start = new Date().getTime();
+let fastestTime = Infinity;
+let firstClick = true;
+let audio = new Audio("sounds/confetti.mp3");
+
+//generate a random hex color code from the string
+function randColor() {
+  var col = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (i = 0; i < 6; i++) {
+    color += col[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function triggerConfetti() {
+  const duration = 3 * 1000;
+  const animationEnd = Date.now() + duration;
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 100 * (timeLeft / duration);
+
+    window.confetti({
+      particleCount: particleCount,
+      spread: randomInRange(100, 200),
+      origin: { x: Math.random(), y: Math.random() - 0.2 },
+    });
+  }, 350); 
+}
+
+function ShapeAppear() {
+  var gameContainer = document.getElementById("gameContainer");
+
+  var containerWidth = gameContainer.offsetWidth;
+  var containerHeight = gameContainer.offsetHeight;
+
+  var top = Math.random() * (containerHeight - 200);
+  var left = Math.random() * (containerWidth - 200);
+  var width = Math.random() * 100 + 100;
+
+  var shape = document.getElementById("shape");
+
+  if (Math.random() > 0.5) {
+    shape.style.borderRadius = "50%";
+  } else {
+    shape.style.borderRadius = "0";
+  }
+
+  shape.style.height = width + "px";
+  shape.style.width = width + "px";
+  shape.style.top = top + "px";
+  shape.style.left = left + "px";
+  shape.style.backgroundColor = randColor();
+
+  shape.style.display = "block";
+  start = new Date().getTime();
+}
+
+function AppearAgain() {
+  setTimeout(ShapeAppear, Math.random() * 2000);
+}
+AppearAgain();
+
+function displayNewFastestTime(time) {
+  // Create a div element for displaying the text
+  const newTextDiv = document.createElement("div");
+  newTextDiv.classList.add("new-fastest-time");
+  newTextDiv.textContent = `New fastest: ${time}s`;
+
+  // Position the div on the top right of the body
+  newTextDiv.style.position = "fixed";
+  newTextDiv.style.top = "1.5rem";
+  newTextDiv.style.right = "1.5rem";
+  newTextDiv.style.fontSize = "2rem";
+  newTextDiv.style.color = "#27c93f";
+
+  // Append the div to the body
+  document.body.appendChild(newTextDiv);
+
+  // Remove the div after a certain duration (e.g., 5 seconds)
+  setTimeout(() => {
+    newTextDiv.classList.add("fade-out"); // Apply fade-out class after a short delay
+    setTimeout(() => {
+      newTextDiv.remove(); // Remove the element after the transition ends
+    }, 5000); // Transition duration (same as the opacity transition duration)
+  }, 100);
+}
+
+document.getElementById("shape").onclick = function () {
+  document.getElementById("shape").style.display = "none";
+  var end = new Date().getTime();
+  var time = (end - start) / 1000;
+  document.getElementById("timeTaken").innerHTML = time + "s";
+  // Compare the current time with the fastest time
+  if (firstClick) {
+    fastestTime = time;
+  }
+  if (!firstClick) {
+    if (time < fastestTime) {
+      fastestTime = time; // Update fastest time
+      triggerConfetti();
+      audio.play();
+      $("#timeTaken").css("color", "#27c93f");
+      displayNewFastestTime(time); // Display the "New fastest time" text
+    } else {
+      $("#timeTaken").css("color", "");
+    }
+  }
+  firstClick = false;
+  AppearAgain();
+};
